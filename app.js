ddf49de5b96aa1457f177cd75eda93801e716131
Baldeep Kaur <baldeepsohal41@gmail.com>
2022-08-14 17:06:16 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,127 @@
+
+// gotta have import/export with the 1st one
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'particles.json', function () {
+//     console.log('callback - particles.js config loaded');
+// });
+
+particlesJS('particles-js',
+
+    {
+        "particles": {
+            "number": {
+                "value": 20,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 6,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 15,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 100,
+                "color": "#ffffff",
+                "opacity": 0.3,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 4,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": false
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 250,
+                    "size": 30,
+                    "duration": 1,
+                    "opacity": 8,
+                    "speed": 2
+                },
+                "repulse": {
+                    "distance": 150
+                },
+                "push": {
+                    "particles_nb": 3
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true,
+        "config_demo": {
+            "hide_card": false,
+            "background_color": "#b61924",
+            "background_image": "",
+            "background_position": "50% 50%",
+            "background_repeat": "no-repeat",
+            "background_size": "cover"
+        }
+    }
+
+);
---
+++ index.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+    $(function(){
+        $(document).scroll(function(){
+            var $nav = $(".navbar");
+            var $header = $(".header");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > ($nav.height() + $header.height()));
+        })
+    })
+
+    /* MAKES THE NAVBAR-TOGGLE ON CLICKING A LINK ***************/
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    /* ***************************************************************************************** */
+
+    /* MAKES THE NAVBAR BRAND APPEAR/DISAPPEAR ON THE NAVBAR AFTER A CERTAIN SCROLL */
+
+    var div = $("#divToShowHide");
+
+    var pos = div.position(); // gets the vertical position
+
+    $(window).scroll(function(){
+        var windowpos = $(window).scrollTop();
+
+        //console.log("value of pos.top is: " + pos.top);
+        //console.log("value of windowpos is: " + windowpos);
+
+        if(windowpos >= (pos.top + 100)){
+            div.addClass("afterScroll");
+        }
+        else{
+            div.removeClass("afterScroll");
+        }
+    });
+
+    /* ******************************************************************************************** */
+
+    // FOLLOWING CODE IS TO TRIGGER ANIMATIONS WHEN THE SPECIFIC ELEMENT BECOMES
+    // VISIBLE ON THE SCREEN ******************************
+
+    var scroll = window.requestAnimationFrame ||
+            function(callback){
+                window.setTimeout(callback, 1000/60)
+            };
+
+    // gets all the elements with that class into an array
+    var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+    function loop() {
+        elementsToShow.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                element.classList.add('is-visible');
+            }
+        });
+
+        scroll(loop);
+    }
+    loop();
+
+    function isElementInViewport(el) {
+        // special bonus for those using jQuery
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+        var rect = el.getBoundingClientRect();
+        return (
+            (rect.top <= 0
+                && rect.bottom >= 0)
+                ||
+                (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+                ||
+                (rect.top >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+                );
+            }
+
+
+});
